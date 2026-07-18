5fb49700d3de5e5156d0e76f9350548c46109ccb
Farruxh <farrukh.web2@gmail.com>
2026-07-18 17:17:54 +0500

    Add initial implementation of common DSA patterns

---
+++ code.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //      Pattern 1
+    // int n = 6;
+    // for(int i = 0; i <= n-1; i++){
+    //     for(int j = 0; j < i+1; j++){
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    //      Pattern 2
+    // int n = 4;
+    // for(int i = 0; i < n; i++){
+    //     for(int j = 0; j < i+1; j++){
+    //         cout << (i+1);
+    //     }
+    //     cout << endl;
+    // }
+
+
+    //      Pattern 3 with characters
+    // char ch = 'A';
+    // for(int i=0; i<5; i++){
+    //     for(int j=0; j<i+1; j++){
+    //         cout << ch;
+    //     }
+    //     ch++;
+    //     cout << endl;
+    // }
+
+    //      Pattern 4
+    // int n = 4;
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1; j<i+1; j++){
+    //         cout << j;
+    //     }
+    //     cout << endl;
+    // }
+
+    //     Pattern 5
+    // for(int i=0; i<4; i++){
+    //     for(int j=i+1;j>0;j--){
+    //         cout << j;
+    //     }
+    //     cout << endl;
+    // }
+
+    //     Pattern 6 Floyd's triangle numbers
+    // int num=1;
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<i+1;j++){
+    //         cout << num <<" ";
+    //         num++;
+    //     } 
+    //     cout << endl;
+    // }
+
+    // Pattern 7 Floyed's triangle characters
+    // char ch = 'A';
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<i+1;j++){
+    //         cout<< ch << " ";
+    //         ch++;
+    //     }
+    //     cout << endl;
+    // }
+
+    // Pattern 7 Inverted Triangle
+    for(int i=0;i<4;i++){
+        for(int j=4;j>0;j--){
+            cout<< i+1;
+        }
+        cout<< endl;
+    }
+    return 0;
+}
