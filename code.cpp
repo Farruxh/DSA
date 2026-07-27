cabfeee92bbd5ff595a65c5613297b48394b9ba8
Farruxh <farrukh.web2@gmail.com>
2026-07-27 23:55:02 +0500

    practice bitwise operations

--- code.cpp
+++ code.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+int sum(int a, int b) {
+    return a+b;
+}
+
 int main() {
 
     //      Pattern 1
@@ -118,39 +122,46 @@ int main() {
     // }
 
     //      Pattern 9 Hollow Diamond
-    int n=4;
-    // top part of diamond
-    for(int i=0; i<n; i++){
-        // spaces
-        for(int j=0; j<=n-i-1; j++){
-            cout << " ";
-        }
-        cout << "*";
-        if(i!=0){
-            // spaces
-            for(int j=0; j<2*i-1; j++){
-                cout << " ";
-            }
-            cout << "*";
-        }
-        cout << endl;
-    }
+    // int n=4;
+    // // top part of diamond
+    // for(int i=0; i<n; i++){
+    //     // spaces
+    //     for(int j=0; j<=n-i-1; j++){
+    //         cout << " ";
+    //     }
+    //     cout << "*";
+    //     if(i!=0){
+    //         // spaces
+    //         for(int j=0; j<2*i-1; j++){
+    //             cout << " ";
+    //         }
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
 
     // bottom part of diamond
-    for(int i=0; i<n-1; i++){
-        // spaces
-        for(int j=0; j<i+2; j++){
-            cout<< " ";
-        }
-        cout<< "*";
-        if(i != n-2){
-            // spaces
-            for(int j=0; j<2*(n-i)-5; j++){
-                cout<< " ";
-            }
-            cout << "*";
-        }
-        cout << endl;
-    }
+    // for(int i=0; i<n-1; i++){
+    //     // spaces
+    //     for(int j=0; j<i+2; j++){
+    //         cout<< " ";
+    //     }
+    //     cout<< "*";
+    //     if(i != n-2){
+    //         // spaces
+    //         for(int j=0; j<2*(n-i)-5; j++){
+    //             cout<< " ";
+    //         }
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    int a=4, b=8;
+    cout << (a & b) << endl;  // bitwise AND
+    cout << (a | b) << endl;  // bitwise OR
+    cout << (a ^ b) << endl;  // bitwise XOR
+    cout << (4 << 1) << endl; // bitwise left shift
+    cout << (4 >> 1) << endl; // bitwise right shift 
     return 0;
 }
